47e146d6b236e2f48dc61a07344b0333144d61d5
jonasbryde <162447342+jonasbryde@users.noreply.github.com>
2024-04-26 13:55:43 +0200

    Update optimizeMultiRotor.py

--- optimizeMultiRotor.py
+++ optimizeMultiRotor.py
@@ -1,8 +1,10 @@
 from MinimumDistanceMultiRotor import MinimumDistanceMultiRotor
 from positionMultiRotor import positionMultiRotor
+from sectorAEP import sectorAEP
 
 def optimizeMultiRotor(f,A,k, wd, rows, collumns, boundaries, n_mr):
     minimumDistance = MinimumDistanceMultiRotor(f,A,k, wd, rows, collumns)
     position = positionMultiRotor(boundaries, minimumDistance, n_mr)
+    AEP = sectorAEP(f,A,k,wd,ti,centers,my_wt)
     
-    return position
+    return position, AEP
