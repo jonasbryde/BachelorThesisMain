3a066e9d6572fb510f7085535f09a264051a4c10
jonasbryde <162447342+jonasbryde@users.noreply.github.com>
2024-04-26 13:57:06 +0200

    Create sectorAEP.py

---
+++ sectorAEP.py
@@ -0,0 +1,49 @@
+from py_wake import NOJ
+import xarray as xr
+from py_wake.site import XRSite
+import GenerateMultiRotorCoordinates
+
+
+def sectorAEP(f, A, k, wd, ti, centers, my_wt):
+
+    #list of AEPs for each sector
+    AEP_list = []
+
+    # Loop through each position of A and k
+    for i in range(len(A)):
+        # Create new lists with zeros
+        new_f = [0] * len(f)
+
+        #only the current position to its value
+        new_f[i] = 1
+
+        #Define the site object
+        site = XRSite(
+            ds=xr.Dataset(data_vars={'Sector_frequency': ('wd', new_f), 'Weibull_A': ('wd', A), 'Weibull_k': ('wd', k), 'TI': ti},
+                            coords={'wd': wd}))
+
+
+        #Receiving coordinates from the function GenerateMultiRotorCoordinates in the file GenerateMultiRotorCoordinates.py
+        coordinates = GenerateMultiRotorCoordinates.generateMultiRotorCoordinates(centers, i)
+
+        x_cord = coordinates["xCoordinates"]
+        y_cord = coordinates["yCoordinates"]
+        z_cord = coordinates["zCoordinates"]
+
+        #creating the wind farm model
+        noj = NOJ(site, my_wt)
+
+        # Simulating results from the wind farm model and turbine coordinates
+        simulationResult = noj(x_cord, y_cord, z_cord)
+
+        # Calculating AEP
+        aep = simulationResult.aep()
+
+        # new list with new AEPs
+        AEP_list.append((round(aep.sum().item(), 2)))
+
+    total_aep = sum(AEP_list)
+
+    avg_aep = round(total_aep / len(AEP_list), 2)
+
+    return(avg_aep)
